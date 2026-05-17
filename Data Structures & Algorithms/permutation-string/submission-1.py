974695599687d9810e2109ba601f241940dd091c
GurneckJohal <25068215+GurneckJohal@users.noreply.github.com>
2026-05-16 19:33:06 -0600

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+        
+        s1_counts = [0] * 26
+        for char in s1:
+            s1_counts[ord(char) - ord('a')] += 1
+        
+        s2_counts = [0] * 26
+        l = 0
+        for r, char in enumerate(s2):
+            s2_counts[ord(char) - ord('a')] += 1
+            if (r - l + 1 > len(s1)):
+                s2_counts[ord(s2[l]) - ord('a')] -= 1
+                l+=1
+            if tuple(s2_counts) == tuple(s1_counts):
+                return True
+        return False
